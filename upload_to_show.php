3444d77be10925c0bdc08eda5e7da3c92f08c521
beckpalmx77 <beckpalmx@gmail.com>
2024-08-12 16:00:34 +0700

    Update upload_to_show.php

--- upload_to_show.php
+++ upload_to_show.php
@@ -10,23 +10,56 @@ if (isset($_GET['filename'])) {
         display: table;
         width: 100%;
         text-align: center;
+        background-color: #f0f0f0;
     }
-    img {
-        vertical-align: middle;
-        display: inline-block;
+    .top-panel {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background-color: #333;
+        color: white;
+        padding: 10px;
+        text-align: right;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+    }
+    .top-panel button {
+        background-color: #fa3c3c;
+        border: none;
+        color: white;
+        padding: 5px 10px;
+        cursor: pointer;
+        font-size: 16px;
+        border-radius: 3px;
+    }
+    .top-panel button:hover {
+        background-color: #ff0000;
     }
     .center-container {
         display: table-cell;
         vertical-align: middle;
+        margin-top: 50px; /* เพิ่ม margin-top เพื่อให้ไม่ทับกับ Panel ด้านบน */
+    }
+    img {
+        vertical-align: middle;
+        display: inline-block;
+        max-width: 100%;
+        height: auto;
     }
 </style>
 <body>
+<div class="top-panel">
+    <button onclick="window.close()">ปิด (Close)</button>
+</div>
 <div class="center-container">
-    <img src="<?php echo $_GET['filename']; ?>">
+    <img src="<?php echo htmlspecialchars($_GET['filename']); ?>">
 </div>
 
 <?php } else { ?>
 
+    <div class="top-panel">
+        <button onclick="window.close()">ปิด (Close)</button>
+    </div>
     <div class="center-container">
         <img src="img/file_not_found.png">
     </div>

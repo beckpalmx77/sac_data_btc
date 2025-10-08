479af4e4cd0f8ae0e4b1064ac82bf847dec1e920
beckpalmx77 <beckpalmx@gmail.com>
2025-10-08 11:22:51 +0700

    up

--- show_history_customer_data_detail.php
+++ show_history_customer_data_detail.php
@@ -98,7 +98,7 @@ if (strlen($_SESSION['alogin']) == "") {
                                                        value=""/>
                                                 <button type="button" class="btn btn-danger"
                                                         id="btnClose">Close <i
-                                                            class="fa fa-window-close"></i>
+                                                            class="fa fa-times"></i>
                                                 </button>
                                             </div>
                                         </form>
@@ -198,7 +198,7 @@ if (strlen($_SESSION['alogin']) == "") {
                                         <input type="hidden" name="action" id="action" value=""/>
                                         <button type="button" class="btn btn-danger"
                                                 data-dismiss="modal">Close <i
-                                                    class="fa fa-window-close"></i>
+                                                    class="fa fa-times"></i>
                                         </button>
                                     </div>
                                 </form>
